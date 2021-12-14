4fe9d506abedb3f839cea66853e7a198b7c33b09
mesha <meshach646@gmail.com>
2021-12-13 23:47:44 -0400

    spinner on statistic is working, just got to add the way to calculate the profits

--- app/src/main/java/com/example/storemanager/ui/statistics/StatisticFragment.java
+++ app/src/main/java/com/example/storemanager/ui/statistics/StatisticFragment.java
@@ -4,18 +4,71 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.storemanager.R;
 
-public class StatisticFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+public class StatisticFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_statistics, container, false);
 
+        // Spinner element
+        Spinner spinner = view.findViewById(R.id.spinner);
+
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<>();
+        categories.add("January");
+        categories.add("February");
+        categories.add("March");
+        categories.add("April");
+        categories.add("May");
+        categories.add("June");
+        categories.add("July");
+        categories.add("August");
+        categories.add("September");
+        categories.add("November");
+        categories.add("December");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+
         return view;
     }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+    }
+
 }
