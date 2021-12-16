9bdd442c6f1faa125deb9018948ef995d0d95e13
mesha <meshach646@gmail.com>
2021-12-16 19:55:57 -0400

    hmmm add funds, the nav icon there just got to add the layout to onclick and send it back to me plz

---
+++ app/src/main/java/com/example/storemanager/ui/add_meals/FundsFragment.java
@@ -0,0 +1,30 @@
+package com.example.storemanager.ui.add_meals;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.storemanager.R;
+
+
+public class FundsFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+       View view = inflater.inflate(R.layout.fragment_funds, container, false);
+
+       return view;
+    }
+}
