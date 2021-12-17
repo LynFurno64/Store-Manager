eb6c2a05dae34e3f2b70d3bba2d8abc496f9a3a7
LynFurno64 <66757168+LynFurno64@users.noreply.github.com>
2021-12-17 13:22:15 -0400

    Update Meal Scheme

--- app/src/main/java/com/example/storemanager/database/SQLDatabase/MealDbSchema.java
+++ app/src/main/java/com/example/storemanager/database/SQLDatabase/MealDbSchema.java
@@ -1,12 +1,14 @@
 package com.example.storemanager.database.SQLDatabase;
 
 public class MealDbSchema {
-    public static final class AddMeal{
+    public static final class MealTable{
         public static final String NAME = "Meals";
 
         public static final class Cols{
-            public static final String MealName ="mealname";
-            public static final String MealCost = "mealcost";
+            public static final String UUID = "uuid";
+            public static final String MealName ="name";
+            public static final String MealCost = "price";
+            public static final String TimesOrder = "timesOrder";
         }
     }
 }
